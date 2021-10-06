SubhenduMishra29/blendercompatibe

Animation: support filtering for curves that don't match the action cycle.
Since the looping behavior is defined per curve rather than at
action level, it is possible for curve loop periods to get out of
sync with each other. This commit adds an option to compare curves
against the frame range specified in the action, and treat any
mismatches as errors for the purpose of F-Curve filtering.

When enabled, the check verifies that curves within the action
have valid cyclic extrapolation, the action period evenly
divides by the curve period (since a curve looping at e.g. half
of the action period length still repeats in sync with the action),
and the end values match if the cycle is supposed to be perfect.

Differential Revision: https://developer.blender.org/D11803

release/scripts/startup/bl_ui/space_dopesheet.py
@@ -715,6 +715,10 @@ class DOPESHEET_PT_action(Panel):
 
         col.prop(action, "is_cyclic")
 
+        row = col.row()
+        row.active = action.is_cyclic
+        row.prop(action, "use_cyclic_errors")
+
 
 class LayersDopeSheetPanel:
     bl_space_type = 'DOPESHEET_EDITOR'

jcaballes1/blender

release/scripts/startup/bl_ui/space_dopesheet.py
@@ -715,6 +715,10 @@ class DOPESHEET_PT_action(Panel):
 
         col.prop(action, "use_cyclic")
 
+        row = col.row()
+        row.active = action.use_cyclic
+        row.prop(action, "use_cyclic_errors")
+
 
 class LayersDopeSheetPanel:
     bl_space_type = 'DOPESHEET_EDITOR'